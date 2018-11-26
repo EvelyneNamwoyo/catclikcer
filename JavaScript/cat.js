00bf2bc919a52ac8af4579a620f80639f3e5b56d
Evelyne Namwoyo <evelyne@laboremus.no>
2018-11-26 16:19:22 +0300

    Add event listener to second cat

--- JavaScript/cat.js
+++ JavaScript/cat.js
@@ -1,6 +1,16 @@
 let cat = document.getElementById('mycat')
 let count = 0;
+let catName = 'Kitty';
+let cat2Name = 'Max';
 cat.addEventListener('click', function(){
     count += 1;
     document.getElementById('clicks').innerText = count;
+});
+
+let cat2 = document.getElementById('cat2');
+let count2 = 0;
+cat2.addEventListener('click', function(){
+    console.log('hey')
+    count2 += 1;
+    document.getElementById('clicks2').innerText = count2;
 });
